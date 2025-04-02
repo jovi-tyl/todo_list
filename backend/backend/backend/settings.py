b194d15e58209b8653aedc89f625e26db32858a1
jovityl <jovityldev@gmail.com>
2025-04-02 15:32:05 +0800

    Fix rest_framework in installed_apps

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -41,9 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangorestframework',
+    'rest_framework',
     'corsheaders',
-    'task'
 ]
 
 MIDDLEWARE = [
