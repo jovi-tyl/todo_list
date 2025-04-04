5f0ea922e4be4af9e36f240d577af81c8735006c
jovityl <jovityldev@gmail.com>
2025-04-04 16:47:18 +0800

    Allow React URL in CORS

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -136,5 +136,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # React development server
+    "http://localhost:5173",  # React development server
 ]
