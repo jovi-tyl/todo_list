4ca8df840b0f2dbe816a05c5a48017978c581f6c
jovityl <jovityldev@gmail.com>
2025-04-04 14:40:46 +0800

    Add api.jsx logic

---
+++ frontend/frontend/src/utils/api.jsx
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const api = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {"Content-Type":"application/json"},
+});
+
+const getTasks = async () => {
+    const response = await api.get('/');
+    return response.data;
+};
+    
+const addTask = async () => {
+    const response = await api.post('/', task);
+    return response.data;
+};
+
+const deleteTask = async () => {
+    const response = await api.delete(`/${id}`);
+    return response.data;
+};
+
+export { getTasks, addTask, deleteTask };
