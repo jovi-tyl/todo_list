b627ebc3773d550cdc135e62918d5a205bb4eb65
jovityl <jovityldev@gmail.com>
2025-04-04 14:53:50 +0800

    Add TaskItem logic

---
+++ frontend/frontend/src/components/TaskItem.jsx
@@ -0,0 +1,25 @@
+import { deleteTask } from "../utils/api";
+
+const TaskItem = ({task, onDelete}) => {
+    const handleDelete = async () => {
+        try {
+            await deleteTask(task.id)
+            onDelete(task.id)
+        } catch (error) {
+          console.error("Error detected", error);  
+        }
+
+    return (
+        <div>
+            Title: {task.title}
+            Description: {task.description}
+            Completed:{task.completed ? "Yes" : "No"}
+            <button onClick={handleDelete}>Delete</button>
+        </div>
+    );
+    
+    };
+};
+
+
+export default TaskItem
